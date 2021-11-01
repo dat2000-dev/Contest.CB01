11c1730e463f6809db2792ed04c16728a3581401
dat2000-dev <53572868+dat2000-dev@users.noreply.github.com>
2021-11-01 09:02:19 +0700

    Create Bai3.cpp

---
+++ Contest2/Bai3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9+7;
+
+int main()
+{
+	 int n,k;
+    cin >> n >> k;
+    vector<int> dp(n+1,0);
+    dp[0]=1;
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=min(i,k);++j){
+            dp[i]+=dp[i-j];
+            dp[i]%=mod;
+        }
+    }
+    cout << dp[n];
+	 return 0;
+}
